403b25e30a51361c7fcfb9cab2393e7001019945
TatianaYudina87 <tanyamaz8@gmail.com>
2024-12-02 08:31:22 +0300

    Add files via upload

--- module_1_3.py
+++ module_1_3.py
@@ -2,7 +2,9 @@ name = "Tatiana"
 print(name, type(name))
 name = Age = 37
 print(name, type(name))
-name = new_age = 38
+name = new_age = (37 + 2)
+print(name, type(name))
+name = "is_student"
 print(name, type(name))
 name = 'is_student' == 'is_student'
 print(name, type(name))
